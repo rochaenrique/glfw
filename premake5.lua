3c34db3540f8347142f52e7e9ce29ba3efabf701
Enrique Rocha Benatti <rochabenattienrique@gmail.com>
2024-07-08 09:42:25 -0300

    Working GLFW abstractino with updated premake scripts

--- premake5.lua
+++ premake5.lua
@@ -7,15 +7,27 @@ project "glfw"
 
 	files
 	{
-		"include/glfw/glfw3.h",
-		"include/glfw/glfw3native.h",
-		"src/glfw_config.h",
+		"include/GLFW/glfw3.h",
+		"include/GLFW/glfw3native.h",
+		"src/internal.h",
+		"src/platform.h",
+		"src/mappings.h",
 		"src/context.c",
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
+		"src/platform.c",
 		"src/vulkan.c",
-		"src/window.c"
+		"src/window.c",
+		"src/egl_context.c",
+		"src/osmesa_context.c",
+		"src/null_platform.h",
+		"src/null_joystick.h",
+		"src/null_init.c",
+
+		"src/null_monitor.c",
+		"src/null_window.c",
+		"src/null_joystick.c"
 	}
 
 	filter "system:windows"
@@ -25,9 +37,12 @@ project "glfw"
 		files
 		{
 			"src/win32_init.c",
+			"src/win32_module.c",
 			"src/win32_joystick.c",
 			"src/win32_monitor.c",
+			"src/win32_time.h",
 			"src/win32_time.c",
+			"src/win32_thread.h",
 			"src/win32_thread.c",
 			"src/win32_window.c",
 			"src/wgl_context.c",
@@ -37,14 +52,14 @@ project "glfw"
 
 		defines 
 		{ 
-			"_glfw_win32",
-			"_crt_secure_no_warnings"
+			"_GLFW_WIN32",
+			"_CRT_SECURE_NO_WARNINGS"
 		}
-	
-	filter "configurations:debug"
-		runtime "degub" 
+
+	filter "configurations:Debug"
+		runtime "Debug"
 		symbols "on"
-	
-	filter "configurations:release"
-		runtime "release" 
+
+	filter "configurations:Release"
+		runtime "Release"
 		optimize "on"
